182d504dcfb9ff6be620ba44b1449664612582bc
3lVv0w <krseisenh@gmail.com>
2019-10-14 18:20:46 +0700

    code refactoring

--- lib/inputstorepage.dart
+++ lib/inputstorepage.dart
@@ -21,69 +21,65 @@ class _InputStorePageState extends State<InputStorePage> {
           backgroundColor: Colors.green[300],
           title: Text('Input Data'),
         ),
-        body: SafeArea(top: false,
-        bottom: false,
-        child: Form(
-          key: _formKey,
-          child: ListView(
-          children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.account_balance),
-                      hintText: 'กรุณากรอกชื่อร้าน',
-                      labelText: 'ชื่อร้าน',
-                    ),
+        body: SafeArea(
+          top: false,
+          bottom: false,
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.account_balance),
+                    hintText: 'กรุณากรอกชื่อร้าน',
+                    labelText: 'ชื่อร้าน',
                   ),
-                  Row(
-                  children: <Widget>[
+                ),
+                Row(children: <Widget>[
                   Icon(Icons.beenhere),
                   Text('    ประเภทร้านอาหาร                  '),
                   DropdownButton<String>(
-                        value: dropdownValue,
-                        onChanged: (String newValue) {
-                        setState(() {
-                          dropdownValue = newValue;
-                        });
-                        },
-                        items: <String>[
-                          'ปิ้งย่าง',
-                          'ร้านอาหาร',
-                          'ร้านส้มตำ',
-                          'ร้านกาแฟ',
-                          'ร้านน้ำชา'
-                        ].map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                  ]
+                    value: dropdownValue,
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items: <String>[
+                      'ปิ้งย่าง',
+                      'ร้านอาหาร',
+                      'ร้านส้มตำ',
+                      'ร้านกาแฟ',
+                      'ร้านน้ำชา'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
                   ),
-                  Row(
-                  children: <Widget>[
+                ]),
+                Row(children: <Widget>[
                   Icon(Icons.image),
                   Text(
                     '    รูปร้าน',
                   ),
-                  ]),
-                  Row(
-                  children: <Widget>[
+                ]),
+                Row(children: <Widget>[
                   Icon(Icons.location_searching),
                   Text(
                     '    ตำแหน่งที่ตั้ง',
                   ),
-                  ]),
-                  
-                  Container(
-                      padding: EdgeInsets.only(),
-                      child: RaisedButton(
-                        child: Text('Submit'),
-                        onPressed: null,
-                      )),
-                ],
-        ),
-        ),)
-    );
+                ]),
+                Container(
+                    padding: EdgeInsets.only(),
+                    child: RaisedButton(
+                      child: Text('Submit'),
+                      onPressed: null,
+                    )),
+              ],
+            ),
+          ),
+        ));
   }
 }
